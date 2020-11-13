55ffe67add6734a8bc5549a7fe21e063f7c5edfb
Sulav Acharya <sacharya@infiniteranges.com>
2020-11-12 19:37:57 -0500

    Implemented Check Mode for Keyfactor Role

--- modules/keyfactor/keyfactor_role.py
+++ modules/keyfactor/keyfactor_role.py
@@ -84,13 +84,14 @@ def run_module():
 
     module = AnsibleKeyfactorModule(
         argument_spec=argument_spec,
-        supports_check_mode=False
+        supports_check_mode=True
     )
 
     # if the user is working with this module in only check mode we do not
     # want to make any changes to the environment, just return the current
     # state with no modifications
     if module.check_mode:
+        result['changed'] = checkMode(module)
         module.exit_json(**result)
 
     if module.params['state'] == 'absent':
@@ -102,15 +103,51 @@ def run_module():
 
 import json
 
+def checkMode(module):
+    current = handleGetMode(module)
+    if module.params['state'] == 'absent':
+        if current:
+            return compareState(current, module)
+        return False
+    if module.params['state'] == 'present':
+        if current:
+            return compareState(current, module)
+        return True
+
+def createRequestedState(module):
+    return { 
+        "name": module.params['name'], 
+        "description": module.params['description'],
+        "identities": [i.capitalize() for i in module.params['identities']],
+        "permissions": [i.capitalize() for i in module.params['permissions']],
+        }
+
+def compareState(current, module):
+    requested = createRequestedState(module)
+    # This could be an unordered list. Sets do not preserve order
+    requested["identities"] = set(requested["identities"])
+    requested["permissions"] = set(requested["permissions"])
+    # Valid is currently unused
+    current.pop('Valid')
+    current = {k.lower():v for (k,v) in current.items()}
+    # This could be an unordered list. Sets do not preserve order
+    current["identities"] =  {i.encode('ascii').strip().capitalize() for i in current["identities"].split(',')}
+    current["permissions"] =  {i.encode('ascii').strip().capitalize() for i in current["permissions"].split(',')}
+
+    for key, value in requested.items():
+        if value != current.get(str(key)):
+            return True
+    return False
+
+
 def handleStatePresent(module):
-    current = handleGet(module)
-    if 'Valid' in current:
-        # TODO: compare to see if edit is needed, return false if not
+    current = handleGetMode(module)
+    if compareState(current, module):
         return handleUpdate(module)
     return handleAdd(module)
 
 def handleAdd(module):
-    endpoint = '/CMSAPI/Security/1/AddRole'
+    endpoint = 'CMSAPI/Security/1/AddRole'
     payload = { 
         "name": module.params['name'], 
         "description": module.params['description'], 
@@ -136,7 +173,7 @@ def handleStateAbsent(module):
     return handleDelete(module)
 
 def handleDelete(module):
-    endpoint = '/CMSAPI/Security/1/DeleteRole'
+    endpoint = 'CMSAPI/Security/1/DeleteRole'
     payload = { "name": module.params['name']}
     resp, info = module.handleRequest("POST", endpoint, payload)
     try:
@@ -153,7 +190,7 @@ def handleDelete(module):
         module.fail_json(msg='Failed.')
 
 def handleGet(module):
-    endpoint = '/CMSAPI/Security/1/EditRole'
+    endpoint = 'CMSAPI/Security/1/EditRole'
     payload = { "name": module.params['name']}
     resp, info = module.handleRequest("POST", endpoint, payload)
     try:
@@ -172,7 +209,7 @@ def handleGet(module):
 
 def handleUpdate(module):
     #"Cannot edit Role because it does not exist."
-    endpoint = '/CMSAPI/Security/1/EditRole'
+    endpoint = 'CMSAPI/Security/1/EditRole'
     payload = { 
         "name": module.params['name'], 
         "description": module.params['description'],
@@ -194,6 +231,23 @@ def handleUpdate(module):
             return False
         module.fail_json(msg=message)
 
+def handleGetMode(module):
+    endpoint = 'CMSAPI/Security/1/GetRoles/'
+    resp, info = module.handleRequest("GET", endpoint)
+    try:
+        content = resp.read()
+        contentSet = json.loads(content)
+        collection = [collection_content for collection_content in contentSet if collection_content['Name'] == module.params['name']]
+        if collection:
+            collection = next(iter(collection))
+        return collection
+        module.fail_json(msg='Failed.')
+    except AttributeError:
+        content = info.pop('body', '')
+        message = json.loads(content)['Message']
+        if message == 'Role with Name \'' + module.params['name'] + '\' does not exist.':
+            return {}
+        module.fail_json(msg=message)
 
 def main():
     run_module()
