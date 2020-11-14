3c86fa8372f51e5ea866da3005363a607983f883
sacharya-inf <72032203+sacharya-inf@users.noreply.github.com>
2020-11-13 20:52:02 -0500

    Check mode keyfactor metadata (#9)
    * Implement Keyfactor Meta Checkmode
    * Updated Description

--- modules/keyfactor/keyfactor_metadata_fields.py
+++ modules/keyfactor/keyfactor_metadata_fields.py
@@ -31,6 +31,54 @@ options:
         description:
             - Description of metadata field.  Required if present
         required: false
+    data_type:
+        description:
+            - Data type of the metadata field. Required if present
+            - choices:
+                - 0 : string
+                - 1 : integer
+                - 2 : datetime
+                - 3 : boolean
+                - 4 : multi-value
+                - 5 : big text
+                - 6 : ip address
+                - 7 : binary
+        required: false
+    hint:
+        description: 
+            - Addition field to add hints
+        required: false
+    validation:
+        description: 
+            - Regular expression defination for the content
+        required: false
+    enrollment:
+        description: Add enrollment handling states
+            - choices:
+                - 0: Optional
+                - 1: Required
+                - 2: Hidden
+    options:
+        description: 
+            - Options for datatype 4, multiple choice
+        required: false
+    default:
+        description:
+            - Default value for the meta field
+            - Not supported for data type 2 (datetime) & 5 (bigtext)
+        required: false
+    explicit_update:
+        description:
+            - Require flag in Legacy API to overwrite values. Default false
+        required: false
+    allow_api:
+        description:
+            - Allow Legacy API to get and set values. Default false
+        required: false
+    display_order:
+        description:
+            - Order in which the fields apprear. Default 0
+        required: false
 
 extends_documentation_fragment:
     - keyfactor
@@ -83,13 +131,14 @@ def run_module():
 
     module = AnsibleKeyfactorModule(
         argument_spec=argument_spec,
-        supports_check_mode=False
+        supports_check_mode=True
     )
 
     # if the user is working with this module in only check mode we do not
     # want to make any changes to the environment, just return the current
     # state with no modifications
     if module.check_mode:
+        result['changed'] = checkMode(module)
         module.exit_json(**result)
 
     if module.params['state'] == 'absent':
@@ -100,6 +149,18 @@ def run_module():
     module.exit_json(**result)
 
 import json
+
+def checkMode(module):
+    current = handleGet(module)
+    if module.params['state'] == 'absent':
+        if current:
+            return True
+        return False
+    if module.params['state'] == 'present':
+        if current:
+            requestedState = createRequestedState(module)
+            return compareState(current,requestedState)
+        return True
 
 def handleStatePresent(module):
     current = handleGet(module)
@@ -137,7 +198,7 @@ def compareState(currentState, requestedState):
 
 def handleAdd(module, payload):
     url = module.params.pop('src')
-    endpoint = url+'/MetadataFields'
+    endpoint = url+'/MetadataFields/'
     resp, info = module.handleRequest("POST", endpoint, payload)
     try:
         content = resp.read()
@@ -190,7 +251,7 @@ def handleGet(module):
 
 def handleUpdate(module, payload):
     url = module.params.pop('src')
-    endpoint = url+'MetadataFields/'
+    endpoint = url+'/MetadataFields/'
 
     resp, info = module.handleRequest("PUT", endpoint, payload)
     try:
